4cd2169923c18852787c66872aa379047c027647
Lincoln Teixeira <lincoln@vexter.com.br>
2020-06-14 12:16:04 -0300

    fix home style after add navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,15 +8,16 @@ import './styles.css'
 function Home () {
     return(
        <div className="main-container">
-           <img src={logo} alt="logo"/>
-           <section>
-                <div className="actions-container">
-                    {/*Merda*/}
-                    <Link to="/vehicle/register" className="button">Cadastrar veículos</Link>                    
-                    <Link to="/vehicles/list" className="button">Listar veículos</Link>                    
-                    <Link to="/drivers/list" className="button">Listar motoristas</Link>                    
-                </div>
-           </section>
+           <div className="menu-container">
+                <img src={logo} alt="logo"/>
+                <section>
+                    <div className="actions-container">
+                        <Link to="/vehicle/register" className="button">Cadastrar veículos</Link>                    
+                        <Link to="/vehicles/list" className="button">Listar veículos</Link>                    
+                        <Link to="/drivers/list" className="button">Listar motoristas</Link>                    
+                    </div>
+                </section>
+           </div>
        </div>
     );
 }
